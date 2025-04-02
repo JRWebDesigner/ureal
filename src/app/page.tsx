59f65f6f7fd972db0c65928a5b93fb27cca772db
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-01 21:48:55 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,10 @@ import Hero from '../components/Hero/Hero';
     <>
       <Hero />
       <Container>
+       <div className='flex justify-center items-center flex-col'>
         <h2 className='text-center text-2xl'>Carreras de Pregrado</h2>
         <span className='w-full text-center text-xm'>Licenciatura</span>
+       </div> 
       </Container>
     </>
   );
