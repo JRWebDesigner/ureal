9e9ee764ece446b9deb51315ee6fb8e86477cc39
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-01 22:07:34 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Container from '../components/Container';
 import Hero from '../components/Hero/Hero';
+import List from '../components/List'
  export default function Home() {
   return (
     <>
@@ -9,6 +10,8 @@ import Hero from '../components/Hero/Hero';
         <h2 className='text-center text-2xl font-semibold'>Carreras de Pregrado</h2>
         <span className='w-full text-center text-xm'>Licenciatura</span>
        </div> 
+<List/>
+
       </Container>
     </>
   );
