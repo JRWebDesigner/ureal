deda7c1bd72c42c9ef42a0c96cf40491078a9159
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 11:35:40 -0400

    Update PregradoCarrusel.tsx

--- src/components/Pregrado/PregradoCarrusel.tsx
+++ src/components/Pregrado/PregradoCarrusel.tsx
@@ -1,6 +1,5 @@
 "use client"
 import {motion} from 'framer-motion'
-import React, { useRef, useState } from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
