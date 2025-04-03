c5fdfaf97a7b2ae27f7579b3931ab9a5cea82b26
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 17:12:14 -0400

    Update Elegirnos.tsx

--- src/components/Elegirnos.tsx
+++ src/components/Elegirnos.tsx
@@ -39,7 +39,7 @@ export default function Elegirnos(){
             </h3>
           </SwiperSlide>
           <SwiperSlide className="relative bg-black flex justify-center items-center rounded-3xl w-full h-full">
-            <img className="absolute object-contain md:object-cover opacity-50 w-full h-full z-10 rounded-3xl" src='Images/convenio.jpg' alt='mundo empresarial' />
+            <img className="absolute object-cover opacity-50 w-full h-full z-10 rounded-3xl" src='Images/convenios.jpg' alt='mundo empresarial' />
             <h3 className="relative w-full h-full flex-center z-20 font-bold text-white text-xl px-6 text-center">
               Convenios con empresas y oportunidades de networking
             </h3>
