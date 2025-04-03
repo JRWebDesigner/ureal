8f4c2e69069399d0070706b13e89b610b68e0f6c
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 08:19:06 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,17 @@ import List from '../components/List'
     <>
       <Hero />
       <Container>
-       <div className='flex justify-center items-center flex-col md:flex-row'>
+       <sectio  className='flex justify-center items-center flex-col md:flex-row'>
         <div>
-         <h2 className='text-2xl font-semibold'>Bienvenidos a la Universidad Real de Cámara de Comercio</h2>
+         <h2 className='text-2xl font-semibold '>Bienvenidos a la Universidad Real de Cámara de Comercio</h2>
          <p className="">Somos una institución de educación superior enfocada en formar líderes empresariales con una visión global e innovadora. Con un enfoque práctico y una sólida red de alianzas comerciales, brindamos a nuestros estudiantes una 
         educación de excelencia alineada con las necesidades del mercado.</p>
         </div>
-        <img className="rouded-full" width={150px} src="./Images/bienvenido.jpg"  alt="bienvenida"/>
-       </div>
+        <img className="rounded-full" width={150px} src="/Images/bienvenido.jpg"  alt="bienvenida"/>
+       </section>    
+       <section >
+        
+       </section>
       </Container>
     </>
   );
