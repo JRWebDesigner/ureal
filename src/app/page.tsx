283116c17e1b77cc77630f6486b1ad98ec0cebc2
JRwebdesing <jowebdesigc@gmail.com>
2025-04-03 11:23:36 -0400

    pregrado

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Container from '../components/Container';
 import Hero from '../components/Hero/Hero';
 import Bienvenida from '../components/Bienvenida'
+import PregradoCarrusel from '../components/Pregrado/PregradoCarrusel.tsx'
 import Elegirnos from '../components/Elegirnos'
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Hero />
       <Container>
         <Bienvenida />
+        <PregradoCarrusel />
         <Elegirnos />
       </Container>
     </>
--- src/components/Bienvenida.tsx
+++ src/components/Bienvenida.tsx
@@ -1,12 +1,15 @@
+"use client"
+import {motion} from "framer-motion"
+
 export default function Bienvenida(){
   return(
-    <section className=' my-10 flex justify-center items-center flex-col md:flex-row'>
-        <div className="w-[90%] md:w-[46%]">
-         <h2 className='text-red-950 text-3xl font-semibold '>Bienvenidos a la Universidad Real de Cámara de Comercio</h2>
-         <p className="text-xl">Somos una institución de educación superior enfocada en formar líderes empresariales con una visión global e innovadora. Con un enfoque práctico y una sólida red de alianzas comerciales, brindamos a nuestros estudiantes una 
+    <section className=' my-16 flex justify-center items-center flex-col md:flex-row'>
+        <motion.div initial={{opacity: 0, x:-60}} whileInView={{opacity: 1, x: 0}} transition={{duration: 0.5}} viewport={{once: true}} className="w-[100%] md:w-[46%]">
+         <h2 className='text-red-950 text-2xl md:text-3xl font-semibold '>Bienvenidos a la Universidad Real de Cámara de Comercio</h2>
+         <p className="text-lg md:text-xl">Somos una institución de educación superior enfocada en formar líderes empresariales con una visión global e innovadora. Con un enfoque práctico y una sólida red de alianzas comerciales, brindamos a nuestros estudiantes una 
         educación de excelencia alineada con las necesidades del mercado.</p>
-        </div>
-        <img className="rounded-full w-[300px] h-[300px] md:w-[500px] md:h-[500px] object-cover" src="/Images/bienvenido.jpg"  alt="bienvenida"/>
+        </motion.div>
+        <motion.img initial={{opacity: 0}} whileInView={{opacity:1}} transition={{duration: 0.9}} viewport={{once:true}} className="rounded-full w-[300px] h-[300px] md:w-[500px] md:h-[500px] object-cover" src="/Images/bienvenido.jpg"  alt="bienvenida"/>
     </section>    
   )
 }
