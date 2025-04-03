7c5f2dca71cb9612e49ea48f2c5f88472972f096
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-02 20:30:11 -0400

    Update HeroCarrusel.tsx

--- src/components/Hero/HeroCarrusel.tsx
+++ src/components/Hero/HeroCarrusel.tsx
@@ -1,14 +1,18 @@
 "use client";
 import dynamic from "next/dynamic";
 
-// Cargar Swiper dinámicamente para evitar problemas con SSR
+// Cargar Swiper dinámicamente
 const Swiper = dynamic(() => import("swiper/react").then((mod) => mod.Swiper), { ssr: false });
 const SwiperSlide = dynamic(() => import("swiper/react").then((mod) => mod.SwiperSlide), { ssr: false });
 
 // Importar módulos de Swiper
 import { Autoplay, EffectFade } from "swiper/modules";
+
+// Importar CSS necesario
 import "swiper/css";
 import "swiper/css/effect-fade";
+import "swiper/css/autoplay";
+import { useEffect } from "react";
 
 const slides = [
   { id: 1, bgImage: "/Images/banner.webp" },
@@ -21,9 +25,17 @@ export default function HeroCarrusel() {
     <div className="relative w-full h-full">
       <Swiper
         modules={[Autoplay, EffectFade]}
-        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        autoplay={{
+          delay: 3000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: false,
+          waitForTransition: true,
+        }}
         loop={true}
         effect="fade"
+        fadeEffect={{
+          crossFade: true
+        }}
         speed={1500}
         className="h-full w-full"
       >
