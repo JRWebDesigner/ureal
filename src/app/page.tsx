0abbb4b5d5cb04fa9f96c40c12f81136b624ee03
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 08:22:06 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import List from '../components/List'
          <p className="">Somos una institución de educación superior enfocada en formar líderes empresariales con una visión global e innovadora. Con un enfoque práctico y una sólida red de alianzas comerciales, brindamos a nuestros estudiantes una 
         educación de excelencia alineada con las necesidades del mercado.</p>
         </div>
-        <img className="rounded-full" width={150px} src="/Images/bienvenido.jpg"  alt="bienvenida"/>
+        <img className="rounded-full" width={150} src="/Images/bienvenido.jpg"  alt="bienvenida"/>
        </section>    
        <section >
         
