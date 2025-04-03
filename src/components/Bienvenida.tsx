89155599739909991e5120e03518102f7ff64abc
JRwebdesing <jowebdesigc@gmail.com>
2025-04-03 09:20:28 -0400

    elegirnos

--- src/components/Bienvenida.tsx
+++ src/components/Bienvenida.tsx
@@ -1,7 +1,7 @@
 export default function Bienvenida(){
   return(
     <section className=' my-10 flex justify-center items-center flex-col md:flex-row'>
-        <div className="w-[46%]">
+        <div className="w-[90%] md:w-[46%]">
          <h2 className='text-red-950 text-3xl font-semibold '>Bienvenidos a la Universidad Real de Cámara de Comercio</h2>
          <p className="text-xl">Somos una institución de educación superior enfocada en formar líderes empresariales con una visión global e innovadora. Con un enfoque práctico y una sólida red de alianzas comerciales, brindamos a nuestros estudiantes una 
         educación de excelencia alineada con las necesidades del mercado.</p>
