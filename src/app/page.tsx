28023726a746c4d5cabfaebf44e2c722fc169301
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 08:34:13 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import List from '../components/List'
     <>
       <Hero />
       <Container>
-       <sectio  className='flex justify-center items-center flex-col md:flex-row'>
+       <section className='flex justify-center items-center flex-col md:flex-row'>
         <div>
          <h2 className='text-2xl font-semibold '>Bienvenidos a la Universidad Real de Cámara de Comercio</h2>
          <p className="">Somos una institución de educación superior enfocada en formar líderes empresariales con una visión global e innovadora. Con un enfoque práctico y una sólida red de alianzas comerciales, brindamos a nuestros estudiantes una 
