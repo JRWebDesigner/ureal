dae16863302429be8f2a08ef75f0bc3146a10440
JosueMMDev <162244866+JRWebDesigner@users.noreply.github.com>
2025-04-03 20:15:02 -0400

    Update Oportunidad.tsx

--- src/components/Oportunidad.tsx
+++ src/components/Oportunidad.tsx
@@ -1,6 +1,6 @@
 export default function Oportunidad(){
   return(
-    <div className='bg-red-800 text-white text-3xl w-full py-5 flex justify-center items-center gap-10 my-10'>
+    <div className='bg-red-800 text-white text-xl md:text-3xl w-full py-5 flex justify-center items-center gap-10 my-10'>
       <span className='font-bold'>No pierdas la Oportunidad !!!</span>
       <button className='bg-white text-black rounded-3xl px-5 py-2 cursor-pointer text-xl hover:font-semibold duration-300'>Mas Informacion</button>
     </div>
